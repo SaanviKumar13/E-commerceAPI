ad2b60e1c12d2d561c4af9fdba6b2da6a0edd1d1
SaanviKumar13 <113087236+SaanviKumar13@users.noreply.github.com>
2022-12-05 16:24:45 +0530

    sk/feat/ecommerceapi

---
+++ App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Products from "./components/Products";
+function App(){
+    return <div className="App">
+    <Navbar />
+    <Products />
+    <Footer />
+    </div>;
+}
+
+export default App;
